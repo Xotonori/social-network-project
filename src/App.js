a3f90b0ed148b41324e1a81ba7d03375a886e22a
Xotonori <xotonori29011991@gmail.com>
2020-04-01 16:46:05 +0300

    Lessons 1-7 is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
+};
+
+const Header = () => {
+    return (
+        <div className='Header'>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Messages</a>
+        </div>
+    )
+};
+
+const Technologies = () => {
+    return (
+        <div className="Technologies">
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+};
 
 export default App;
