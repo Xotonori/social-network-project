2ac47069e60914045b68b3584dfa54886a916b4d
Xotonori <xotonori29011991@gmail.com>
2020-04-04 04:55:49 +0300

    create basic html and css

--- src/App.js
+++ src/App.js
@@ -3,34 +3,81 @@ import './App.css';
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Technologies />
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img src={require('./img/logo.png')} alt="logo" />
+            </header>
+            <aside className={'sidebar'}>
+                <div>
+                    <a href="">
+                        Profile
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        Messages
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        News
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        Music
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        Settings
+                    </a>
+                </div>
+            </aside>
+            <main className={'content'}>
+                <img src={require('./img/content-img.jpg')} alt="" className="content__main-image"/>
+                <div className="content__profile">
+                    <img src={require('./img/photo.jpg')} alt="nature" className="content__photo"/>
+                    <div className="content__info">
+                        <div className="content__name">
+                            Alexey D.
+                        </div>
+                        <div className="content__data">
+                            <div className="content__birthday">
+                                Date of Birth: 29 january
+                            </div>
+                            <div className="content__city">
+                                City: Saint-Petersburg
+                            </div>
+                            <div className="content__education">
+                                Education: SPASK'06
+                            </div>
+                            <div className="content__site">
+                                Web site: https://it-kamasutra.com
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="content__my-post">
+                    <h3>My posts</h3>
+                    <textarea name="my-post" id="my-post" cols="30" rows="10">
+                    </textarea>
+                    <button className={'content__send-button'}>Send</button>
+                </div>
+                <div className="content__all-posts">
+                    <div className="content__post">
+                        <img src="" alt="avatar" className="content__avatar-post"/>
+                        <div className="content__text-post">Hey, why nobody love me?</div>
+                    </div>
+                    <div className="content__post">
+                        <img src="" alt="avatar" className="content__avatar-post"/>
+                        <div className="content__text-post">Hey, why nobody love me?</div>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
 
-const Header = () => {
-    return (
-        <div className='Header'>
-            <a href="">Home</a>
-            <a href="">News Feed</a>
-            <a href="">Messages</a>
-        </div>
-    )
-};
-
-const Technologies = () => {
-    return (
-        <div className="Technologies">
-            <ul>
-                <li>html</li>
-                <li>css</li>
-                <li>js</li>
-                <li>react</li>
-            </ul>
-        </div>
-    )
-};
-
 export default App;
+
