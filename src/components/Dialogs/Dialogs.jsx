13b39e8148bafdfb56f14a83e281bb80b1f0dad1
Xotonori <xotonori29011991@gmail.com>
2020-04-11 06:24:49 +0300

    added Routers and NavLinks

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Dialogs.module.css'
+
+const Dialogs = props => {
+    return (
+        <div>
+            Messages
+        </div>
+    );
+};
+
+export default Dialogs;
